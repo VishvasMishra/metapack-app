fa00ba796b7cb0f2368b5d8679e29c361ee02f35
VGM <vishwas.mishra@williamscommerce.com>
2024-02-01 13:22:09 +0530

    Credential update

--- pages/rate/index.tsx
+++ pages/rate/index.tsx
@@ -1,15 +1,8 @@
 import { Box } from "@bigcommerce/big-design";
-import { Button, Dropdown, Panel, Small, Link as StyledLink, Table, TableSortDirection } from '@bigcommerce/big-design';
-import { MoreHorizIcon } from '@bigcommerce/big-design-icons';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { ReactElement, useState } from 'react';
-import ErrorMessage from '../../components/error';
-import Loading from '../../components/loading';
 
 const rate = ()=>{
     return (
-        <Box marginBottom="xxLarge">
+        <Box marginBottom="xxLarge" id="rate">
             <h1>Hello</h1>
         </Box>
     );
