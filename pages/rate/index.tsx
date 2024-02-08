ef77c507fb15e9123e80fa507b37090aa53f710e
VGM <vishwas.mishra@williamscommerce.com>
2024-02-08 18:11:36 +0530

    Responses test aaaaa

--- pages/rate/index.tsx
+++ pages/rate/index.tsx
@@ -1,11 +1,26 @@
-import { Box } from "@bigcommerce/big-design";
+import React, { useEffect, useState } from "react";
+import { useShipping } from "@bigcommerce/storefront-data-hooks";
 
-const Rate = ()=>{
-    return (
-        <Box marginBottom="xxLarge" id="rate">
-            <h1>Hello</h1>
-        </Box>
-    );
-}
+const ShippingProvider = () => {
+  const [shippingMethods, setShippingMethods] = useState([]);
+  const { getShippingMethods } = useShipping();
 
-export default Rate;
+  useEffect(() => {
+    getShippingMethods().then((methods) => {
+      setShippingMethods(methods);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Shipping Methods</h1>
+      <ul>
+        {shippingMethods.map((method) => (
+          <li key={method.id}>{method.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ShippingProvider;
